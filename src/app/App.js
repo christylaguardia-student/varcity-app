b2000af40f6e7591bcf4e5975126bd72270e3a3a
Stephanie Fitzgerald <s.e.v.fitzgerald@gmail.com>
2017-09-18 14:38:00 -0700

    Added select box to media gallery

--- src/app/App.js
+++ src/app/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-import GlobalHeaderContainer from './GlobalHeaderContainer';
+import GlobalHeader from './GlobalHeader';
+// import GlobalHeaderContainer from './GlobalHeaderContainer';
 import GlobalFooter from './GlobalFooter';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <GlobalHeaderContainer />
+        <GlobalHeader />
+        {/* <GlobalHeaderContainer /> */}
         <GlobalFooter />
       </div>
     );
--- src/media/MediaGallery.js
+++ src/media/MediaGallery.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import 'bulma/css/bulma.css';
 import PropTypes from 'prop-types';
-import { TextInput, TextArea, UrlInput } from '../app/FormControls';
-
+import { TextSelect, TextArea, UrlInput } from '../app/FormControls';
 
 GalleryItem.propTypes = {
   description: PropTypes.string.isRequired,
@@ -14,43 +13,53 @@ GalleryItem.propTypes = {
 
 // just do this for now
 function httpCallback() { console.log('pretending to make api call') }
-const value = '';
+let value = '';
 
-export function GalleryItem({ description, videoUrl = '', image = '', mediaType, onUpdate }) {
+export function GalleryItem({ options, select, description, videoUrl = '', image = '', mediaType, onUpdate }) {
+  console.log('select is', select);
+
   return (
-    <div className="galleryView file">
-      {/* <UrlInput className="file-input" type="file" name="item" value={value} propName="image" label="image" change={httpCallback} /> */}
+    <div className="galleryView">
       <img src={image} alt={description} />
-      <TextInput value={value} propName="mediaType" label="Media Type" change={httpCallback}/>
-      <TextArea value={value} propName="description" label="Description" change={httpCallback} />
       {/* <button onClick={() => onRemove(item)} >X</button> */}
+      <div className="is-grouped is-grouped-multiline">
+        <TextSelect className="select" value={select} propName="mediaType" label="Media Type" change={httpCallback} options={options} />
+        {select.text === 'video link' &&
+        <UrlInput value={value} propName="videoUrl" label="Video Link" change={httpCallback} />
+        }
+        {select.text === 'image upload' &&
+        <div className="file">
+          <label className="file-label">
+            <input className="file-input" type="file" name="imageUrl" />
+            <span className="file-cta">
+              <span className="icon file-icon">
+                <i class="fa fa-upload"></i>
+              </span>
+              <span class="file-label">
+                Choose a file...
+              </span>
+            </span>
+          </label>
+        </div>
+        }
+        <TextArea value={value} propName="description" label="Description" change={httpCallback} />
+      </div>
     </div>
   );
 }
 
-export function AddItem({ onAdd }) {
-    return (
-        <div className="file">
-            <form onSubmit={event => {
-                event.preventDefault();
-                const form = event.target;
-                {/* const { title, description, url } = form.elements;
-                onAdd(title.value, description.value, url.value); */}
-                form.reset();
-            }}>
-                <label className="file-label" htmlFor="description">Title:</label>
-                <input className="file-input" type="file" id="description" name="description" required /> 
-                <label htmlFor="description">Description:</label>
-                <input id="description" name="description" required /> 
-                <label htmlFor="url">url:</label>
-                <input id="url" name="url" required /> 
-                <button type="submit">Add Bunny</button>
-            </form>
-        </div>
-    );
-}
-
 export class MediaGallery extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state={
+      select: { id: "0", text: "video link" },
+      selectOptions: [
+        { id: "0", text: "video link" }, 
+        { id: "1", text: "image upload" }
+        ]
+    };
+  }
 
   static propTypes = {
     galleryArray: PropTypes.arrayOf(PropTypes.object).isRequired,
@@ -58,18 +67,18 @@ export class MediaGallery extends Component {
   }
 
   render() {
-    const { galleryArray, onUpdate, itemNum } = this.props;
-    const itemGallery = galleryArray.map((item, i) => (
-      <GalleryItem key={i} item={item} description={item.description} url={item.url} onUpdate={onUpdate} />
-    ));
+    // const { galleryArray, onUpdate, itemNum } = this.props;
+    // const itemGallery = galleryArray.map((item, i) => (
+    //   <GalleryItem key={i} item={item} description={item.description} url={item.url} onUpdate={onUpdate} />
+    // ));
     return (
-      <div className="tile is-vertical">
-        <nav id="galleryNav">
+      <div className="tile">
+        {/* <nav id="galleryNav">
           <button onClick={() => onUpdate(-1)}>&laquo; Previous</button> <button onClick={() => onUpdate(1)}>Next &raquo;</button>
           <p>item {itemNum + 1} of {itemGallery.length}</p>
         </nav>
-        {itemGallery[itemNum]}
-        <AddItem />
+        {itemGallery[itemNum]} */}
+        <GalleryItem select={this.state.select} options={this.state.selectOptions}/>
       </div>
     );
   }
