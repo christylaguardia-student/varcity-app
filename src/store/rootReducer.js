0888a77a19d1c6f61f63530333055b1addf4519f
christylaguardia <christinelaguardia@gmail.com>
2017-09-18 15:58:16 -0700

    added blank sport

--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -1,8 +1,7 @@
 import { combineReducers } from 'redux';
 import { info } from '../info/reducers';
 import { location } from '../info/location/reducers';
-import { app } from '../app/reducers';
-import { authorized } from '../app/reducers'
+import { authorized } from '../app/reducers';
 import { sport } from '../sports/reducers';
 import { edu } from '../edu/reducers';
 import { media } from '../media/reducers';
