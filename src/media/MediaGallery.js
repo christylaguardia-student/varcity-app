9ecc34df5675189b520cc3ed3c48c43873534c15
Stephanie Fitzgerald <s.e.v.fitzgerald@gmail.com>
2017-09-18 15:00:11 -0700

    Added carousel to media gallery

--- src/media/MediaGallery.js
+++ src/media/MediaGallery.js
@@ -3,13 +3,13 @@ import 'bulma/css/bulma.css';
 import PropTypes from 'prop-types';
 import { TextSelect, TextArea, UrlInput } from '../app/FormControls';
 
-GalleryItem.propTypes = {
-  description: PropTypes.string.isRequired,
-  mediaType: PropTypes.string.isRequired,
-  videoUrl: PropTypes.string,
-  image: PropTypes.object,
-  // onRemove: PropTypes.func.isRequired
-}
+// GalleryItem.propTypes = {
+//   description: PropTypes.string.isRequired,
+//   mediaType: PropTypes.string.isRequired,
+//   videoUrl: PropTypes.string,
+//   image: PropTypes.object,
+//   // onRemove: PropTypes.func.isRequired
+// }
 
 // just do this for now
 function httpCallback() { console.log('pretending to make api call') }
@@ -52,33 +52,38 @@ export class MediaGallery extends Component {
 
   constructor(props) {
     super(props);
-    this.state={
+    this.state = {
+      items: [],
+      itemNum: 0,
       select: { id: "0", text: "video link" },
       selectOptions: [
-        { id: "0", text: "video link" }, 
+        { id: "0", text: "video link" },
         { id: "1", text: "image upload" }
-        ]
-    };
+      ]
+    }
   }
 
-  static propTypes = {
-    galleryArray: PropTypes.arrayOf(PropTypes.object).isRequired,
-    // onRemove: PropTypes.func.isRequired
-  }
+    onUpdate(incr) {
+      const itemCount = this.state.items.length;
+      let newItem = this.state.itemNum + incr;
+      if (newItem === itemCount) newItem = 0;
+      else if (newItem === -1) newItem = itemCount - 1;
+      this.setState({ itemNum: newItem });
+    }
 
   render() {
-    // const { galleryArray, onUpdate, itemNum } = this.props;
-    // const itemGallery = galleryArray.map((item, i) => (
-    //   <GalleryItem key={i} item={item} description={item.description} url={item.url} onUpdate={onUpdate} />
+    const { items, onUpdate, itemNum, select, selectOptions } = this.state;
+    // const itemGallery = items.map((item, i) => (
+    //   <GalleryItem key={i} item={item} description={item.description} url={item.url} onUpdate={onUpdate} select={select} options={selectOptions}/>
     // ));
     return (
       <div className="tile">
-        {/* <nav id="galleryNav">
+        <GalleryItem onUpdate={onUpdate} select={select} options={selectOptions}/>
+        <nav id="galleryNav">
           <button onClick={() => onUpdate(-1)}>&laquo; Previous</button> <button onClick={() => onUpdate(1)}>Next &raquo;</button>
-          <p>item {itemNum + 1} of {itemGallery.length}</p>
+          <p>item {itemNum + 1} of {items.length}</p>
         </nav>
-        {itemGallery[itemNum]} */}
-        <GalleryItem select={this.state.select} options={this.state.selectOptions}/>
+        {/* {itemGallery[itemNum]} */}
       </div>
     );
   }
