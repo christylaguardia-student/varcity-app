c5636d38d87f3a1c6c2cc646c07d5a198f87a92a
Stephanie Fitzgerald <s.e.v.fitzgerald@gmail.com>
2017-09-19 00:36:07 -0700

    Values are saved in holdData function for submission through button

--- src/media/MediaContainer.js
+++ src/media/MediaContainer.js
@@ -1,32 +1,8 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
 import 'bulma/css/bulma.css';
 import { MediaGallery } from './MediaGallery';
 
 export class MediaContainer extends Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      items: [],
-      itemNum: 0
-    };
-
-    const onUpdate = function (incr) {
-      const itemCount = this.state.items.length;
-      let newItem = this.state.itemNum + incr;
-      if (newItem === itemCount) newItem = 0;
-      else if (newItem === -1) newItem = itemCount-1;
-      this.setState ({ itemNum: newItem });
-    }
-  }
-
-  // componentDidMount() {
-  //   fetch('/api/athletes/:id/media')
-  //     .then(res => res.json())
-  //     .then(items => this.setState({ items }))
-  //     .catch(error => console.log(error));
-  // }
 
   render() {
     return (
--- src/media/MediaGallery.js
+++ src/media/MediaGallery.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import 'bulma/css/bulma.css';
 import PropTypes from 'prop-types';
-import { TextSelect, TextArea, UrlInput } from '../app/FormControls';
+import { TextSelect, TextArea, UrlInput, Toggle, ToggleEditMode } from '../app/FormControls';
 import { updateMedia, getMedia } from './actions';
 
 // GalleryItem.propTypes = {
@@ -14,8 +14,14 @@ import { updateMedia, getMedia } from './actions';
 // }
 
 // just do this for now
-function httpCallback() { console.log('pretending to make api call') }
 let value = '';
+let mediaItem = { description: '', mediaType: '', videoUrl: '', image: '' };
+function holdData(value) { 
+  let result = '';
+  if(value.mediaType) result = value.mediaType.text;
+  else result = Object.values(value)[0];
+  mediaItem[Object.keys(value)] = result;
+}
 
 export function GalleryItem({ options, select, description, videoUrl = '', image = '', mediaType, rotateGallery }) {
   console.log('select is', select);
@@ -25,9 +31,9 @@ export function GalleryItem({ options, select, description, videoUrl = '', image
       <img src={image} alt={description} />
       {/* <button onClick={() => onRemove(item)} >X</button> */}
       <div className="is-grouped is-grouped-multiline">
-        <TextSelect className="select" value={select} propName="mediaType" label="Media Type" change={httpCallback} options={options} disabled={false} />
+        <TextSelect className="select" value={select} propName="mediaType" label="Media Type" change={holdData} options={options} disabled={false} />
         {select.text === 'video link' &&
-        <UrlInput value={value} propName="videoUrl" label="Video Link" change={httpCallback} disabled={false} />
+        <UrlInput value={value} propName="videoUrl" label="Video Link" change={holdData} disabled={false} />
         }
         {select.text === 'image upload' &&
         <div className="file">
@@ -44,7 +50,8 @@ export function GalleryItem({ options, select, description, videoUrl = '', image
           </label>
         </div>
         }
-        <TextArea value={value} propName="description" label="Description" change={httpCallback} disabled={false} />
+        <TextArea value={value} propName="description" label="Description" change={holdData} disabled={false} />
+        {/* <button onClick={() => onSubmit()} */}
       </div>
     </div>
   );
@@ -55,7 +62,6 @@ export class MediaGallery extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      items: [],
       itemNum: 0,
       select: { id: "0", text: "video link" },
       selectOptions: [
@@ -70,7 +76,7 @@ export class MediaGallery extends Component {
   }
 
   componentDidMount() {
-    // this.props.getMedia();
+    // this.props.getMedia(456);
   }
 
   handleMediaSubmit(media) {
@@ -78,32 +84,38 @@ export class MediaGallery extends Component {
   }
 
   rotateGallery(incr) {
-    const itemCount = this.state.items.length;
-    let newItem = this.state.itemNum + incr;
-    if (newItem === itemCount) newItem = 0;
-    else if (newItem === -1) newItem = itemCount - 1;
-    this.setState({ itemNum: newItem });
+    // const itemCount = this.props.items.length;
+    // let newItem = this.state.itemNum + incr;
+    // if (newItem === itemCount) newItem = 0;
+    // else if (newItem === -1) newItem = itemCount - 1;
+    // this.setState({ itemNum: newItem });
   }
 
   render() {
-    const { items, rotateGallery, itemNum, select, selectOptions } = this.state;
-    const itemGallery = items.map((item, i) => (
-      <GalleryItem key={i} image={item} description={item.description} videoUrl={item.url} mediaType={item.mediaType} rotateGallery={rotateGallery} select={select} options={selectOptions}/>
-    ));
+    const { items } = this.props;
+    const { itemNum, rotateGallery, select, selectOptions } = this.state;
+    // const itemGallery = items.map((item, i) => (
+    //   <GalleryItem key={i} image={item} description={item.description} videoUrl={item.url} mediaType={item.mediaType} rotateGallery={rotateGallery} select={select} options={selectOptions}/>
+    // ));
     return (
-      <div className="tile">
-        {/* <GalleryItem rotateGallery={rotateGallery} select={select} options={selectOptions}/> */}
-        <nav id="galleryNav">
-          <button onClick={() => rotateGallery(-1)}>&laquo; Previous</button> <button onClick={() => rotateGallery(1)}>Next &raquo;</button>
-          <p>item {itemNum + 1} of {items.length}</p>
-        </nav>
-        {itemGallery[itemNum]}
+      <div className="field">
+        <ToggleEditMode value="edit" propName="edit" change={holdData} disabled={false} />
+
+        <div className="tile">
+          <GalleryItem rotateGallery={rotateGallery} select={select} options={selectOptions}/>
+          {/* {itemGallery[itemNum]} */}
+          {/* <nav id="galleryNav">
+            <button onClick={() => rotateGallery(-1)}>&laquo; Previous</button> <button onClick={() => rotateGallery(1)}>Next &raquo;</button>
+            <p>item {itemNum + 1} of {items.length}</p>
+          </nav> */}
+        </div>
       </div>
     );
   }
 }
 
 export default connect(state => {
+  console.log('state in mediagallery is', state);
   return {
     media: state.items,
   };
