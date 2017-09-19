3cd317dea198a8f9c280e62d16f0a520de03e169
Wendy Beck <webeck@gmail.com>
2017-09-19 13:20:45 -0700

    Merge branch 'media' of https://github.com/varcity-network/varcity-app into home+header
    * 'media' of https://github.com/varcity-network/varcity-app:
      Updates to media gallery - file upload
      Values are saved in holdData function for submission through button
      Media actions test pass
      Media reducers tests pass
      Actions, constants for media, addition to athleteAPI
      Added carousel to media gallery
      Added select box to media gallery
      media gallery and container updates
    
    # Conflicts:
    #	src/app/App.js
    #	src/app/GlobalHeader.js
    #	src/app/GlobalHeaderContainer.js

--- src/app/GlobalHeaderContainer.js
+++ src/app/GlobalHeaderContainer.js
@@ -1,40 +1,7 @@
-import React from 'react';
 import { connect } from 'react-redux';
 import GlobalHeader from './GlobalHeader';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-import Home from './Home';
-import SearchContainer from '../search/SearchContainer';
-import MediaContainer from '../media/MediaContainer';
-import ProfileContainer from './ProfileContainer'
 import { signIn, signUp, httpCallback } from './actions';
-// import Home from './Home';
 
-export function GlobalHeaderContainer() {
-  return (
-    <Router>
-      <div>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><input placeholder="search" /></li>
-          <li><Link to="/athletes">Search</Link></li>
-          <li><Link to="/athletes/:id">Profile</Link></li>
-        </ul>
-
-        <Switch>
-          <Route exact path="/" component={MediaContainer} />
-          {/* <Route exact path="/" component={Home} /> */}
-          <Route exact path="/athletes" component={SearchContainer} />
-          <Route exact path="/athletes/:id" component={ProfileContainer} />
-        </Switch>
-      </div>
-      <div>
-        <GlobalHeader />
-      </div>
-    </Router>
-
-
-  );
-}
 function mapDispatchToProps(dispatch) {
   return {
     signUp: (email, password) => {
@@ -42,9 +9,6 @@ function mapDispatchToProps(dispatch) {
     },
     signIn: (email, password) => {
       dispatch(signIn(email, password));
-    },
-    httpCallback: value => {
-      dispatch(httpCallback(value));
     }
   };
 }
@@ -52,7 +16,6 @@ function mapDispatchToProps(dispatch) {
 const mapStateToProps = (state) => {
   return {
     id: state.id,
-    value: 'myvalue',
     authorized: state.authorized
     };
 };
