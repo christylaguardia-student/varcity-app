4313123f14c2f12e009cdea89ffb9bb00b885482
christylaguardia <christinelaguardia@gmail.com>
2017-09-19 11:56:17 -0700

    added _id

--- src/store/athletes/defaultValues.js
+++ src/store/athletes/defaultValues.js
@@ -1,50 +1,52 @@
 
 export default {
-  info: {
-    firstName: '',
-    lastName: '',
-    public: false,
-    profileUrl: '',
-    primarySport: '',
-    position: '',
-    person: {
-      dob: '',
-      gender: '', 
-      height: 0,
-      heightUom: 'in',
-      weight: 0,
-      weightUom: 'lb'
+  _id: {
+    info: {
+      firstName: '',
+      lastName: '',
+      public: false,
+      profileUrl: '',
+      primarySport: '',
+      position: '',
+      person: {
+        dob: '',
+        gender: '', 
+        height: 0,
+        heightUom: 'in',
+        weight: 0,
+        weightUom: 'lb'
+      },
+      organization: '',
+      location: {
+        city: '',
+        state: '',
+        country: '',
+      },
+      socials: {
+        facebookUrl: '',
+        twitterUrl: '',
+        instagramUrl: '',
+      }
     },
-    organization: '',
-    location: {
-      city: '',
-      state: '',
-      country: '',
+    bio: {
+      about: '',
+      awards: '',
     },
-    socials: {
-      facebookUrl: '',
-      twitterUrl: '',
-      instagramUrl: '',
-    }
-  },
-  bio: {
-    about: '',
-    awards: '',
-  },
-  edu: [],
-  testScores: {
-    SAT: {
-      reading: 0,
-      math: 0,
-      writing: 0
+    edu: [],
+    testScores: {
+      SAT: {
+        reading: 0,
+        math: 0,
+        writing: 0
+      },
+      ACT: {
+        reading: 0,
+        math: 0,
+        writing: 0,
+        science: 0
+      }
     },
-    ACT: {
-      reading: 0,
-      math: 0,
-      writing: 0,
-      science: 0
-    }
-  },
-  sports: [],
-  media: []
+    sports: [],
+    media: []
+  }
 };
