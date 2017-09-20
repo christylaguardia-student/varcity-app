67437dbae0e12de604295a42a2976ca668e3938f
christylaguardia <christinelaguardia@gmail.com>
2017-09-19 17:44:43 -0700

    added semis

--- src/app/constants.js
+++ src/app/constants.js
@@ -1,4 +1,4 @@
-export const SIGN_IN = 'SIGN_IN'
-export const SIGN_UP = 'SIGN_UP'
-export const AUTH_FAILURE = 'AUTH_FAILURE'
-export const AUTHORIZED = 'AUTHORIZED'
+export const SIGN_IN = 'SIGN_IN';
+export const SIGN_UP = 'SIGN_UP';
+export const AUTH_FAILURE = 'AUTH_FAILURE';
+export const AUTHORIZED = 'AUTHORIZED';
