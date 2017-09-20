6bd9f220873d7031fe018846ff917c57db4920ef
christylaguardia <christinelaguardia@gmail.com>
2017-09-19 17:44:34 -0700

    added About.js

---
+++ src/app/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <div>
+      <h3>Our Mission is to give student-athletes the power to build community.</h3>
+      <p>The Varcity Network celebrates how our athletic and education identities inspire us, support us, and help us discover the world through community connection.</p>
+      <h3>Contact</h3>
+      <p>Your ideas and suggestions help us to constantly improve Varcity’s features. Let us know how we can improve your experience.</p>
+      <p><a href="info@varcitynetwork.com">info@varcitynetwork.com</a></p>
+    </div>
+  );
+}
