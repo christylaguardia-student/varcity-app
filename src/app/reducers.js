d211911880ea98b4e78b3f9437f25eb76d2f4f4f
Wendy Beck <webeck@gmail.com>
2017-09-20 14:29:54 -0700

    adding new reducer for no token

--- src/app/reducers.js
+++ src/app/reducers.js
@@ -1,6 +1,7 @@
 import {
   AUTHORIZED,
   AUTH_FAILURE,
+  NO_LOCAL_TOKEN,
   RETRIEVE_ID_WITH_TOKEN,
   SIGN_OUT
 } from './constants';
@@ -10,6 +11,8 @@ export function authId(state = {}, { type, payload }) {
     case AUTHORIZED:
       return payload;
     case AUTH_FAILURE:
+      return null;
+    case NO_LOCAL_TOKEN:
       return null;
     case RETRIEVE_ID_WITH_TOKEN:
       return payload;
