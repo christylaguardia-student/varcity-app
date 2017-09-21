44b7ef0cd0777aa682e99e9ebf15dddff3a7764c
Pierre Salumu-Shabani <jrsalshab@outlook.com>
2017-09-20 17:47:52 -0700

    test action

--- src/edu/actions.js
+++ src/edu/actions.js
@@ -1,25 +1,38 @@
 import * as actions from './constants';
-import eduApi from './eduApi';
+import athleteApi from '../services/athleteApi';
 
 
-export function getAllEducation(id) {
-  return dispatch => {
-      dispatch({type: actions.LOAD_EDUCATION});
-      EduApi.get(id)
-          .then(
-              events => {
-                  dispatch({
-                      type: actions.LOAD_EDUCATIONS, payload: educations
-                  });
-              },
-              error => {
-                  dispatch({
-                      type: actions.LOAD_EDUCATIONS_ERROR, payload: error
-                  });
-              }
-          );
+export function makeGetEducation(api) {
+    return function getEducation(id) {
+      return dispatch => {
+        return api
+        .get(id)
+    .then(education => {
+        console.log(education)
+            dispatch({type: actions.LOAD_EDUCATIONS, payload: education});})
+        .catch(console.log);
+    };
+};
+}
+
+export const getEducation = makeGetEducation(athleteApi);
+
+export function makeUpdateEducation(api) {
+  return function updateEducation(id) {
+    return dispatch => {
+      return api
+        .update(id)
+        .then(education => {
+          dispatch({ type: actions.CREATE_NEW_EDUCATION, payload: education });
+        })
+        .catch(console.log);
+    };
   };
 }
+
+export const updateEducation = makeUpdateEducation(athleteApi);
+
+
 
 export const createNewEducation = (education, newId) => {
   return {
@@ -35,61 +48,61 @@ export const createNewEducation = (education, newId) => {
 }
 
 
-export function createEducation(education) {
-    return function(dispatch) {
-        return EduApi.createEducation(education)
-        .then(response => {
-            dispatch(createNewEducationSuccess(response));
-            return response;
-        }).catch(error => {
-            throw(error);
-        });
-    };
-}
+// export function createEducation(education) {
+//     return function(dispatch) {
+//         return EduApi.createEducation(education)
+//         .then(response => {
+//             dispatch(createNewEducationSuccess(response));
+//             return response;
+//         }).catch(error => {
+//             throw(error);
+//         });
+//     };
+// }
 
-export function loadEducationsSuccess(cats) {
-    return {type: actions.LOAD_EDUCATIONS_SUCCESS, educations};
-  }
+// export function loadEducationsSuccess(cats) {
+//     return {type: actions.LOAD_EDUCATIONS_SUCCESS, educations};
+//   }
     
-  export function createCatSuccess(cat) {
-    return {type: actions.CREATE_EDUCATION_SUCCESS, education};
-  }
+//   export function createCatSuccess(cat) {
+//     return {type: actions.CREATE_EDUCATION_SUCCESS, education};
+//   }
   
-  export function deleteCatSuccess(cat) {
-    return {type: actions.DELETE_EDUCATION_SUCCESS, education};
-  }
+//   export function deleteCatSuccess(cat) {
+//     return {type: actions.DELETE_EDUCATION_SUCCESS, education};
+//   }
   
 
-export function loadEducations() {
-    return function(dispatch) {
-        return EduApi.getAllEducation()
-        .then(educations => {
-            dispatch(loadEducationsSuccess(educations));
-        }).catch(error => {
-            throw(error);
-        });
-    };
-}
+// export function loadEducations() {
+//     return function(dispatch) {
+//         return EduApi.getAllEducation()
+//         .then(educations => {
+//             dispatch(loadEducationsSuccess(educations));
+//         }).catch(error => {
+//             throw(error);
+//         });
+//     };
+// }
 
-export function createNewEducationSuccess (education) {
-  return function (dispatch) {
-      return EduApi.createNewEducationSuccess(education).then((response) => {
-          dispatch(createNewEducation(education, response.id))
-      }).catch(error => {
-          throw error
-      });
-  }
-}
+// export function createNewEducationSuccess (education) {
+//   return function (dispatch) {
+//       return EduApi.createNewEducationSuccess(education).then((response) => {
+//           dispatch(createNewEducation(education, response.id))
+//       }).catch(error => {
+//           throw error
+//       });
+//   }
+// }
      
 
-export const deleteEducation = (id) => {
-  return function(dispatch) {
-      return EduApi.deleteEducation(id)
-      .then((res) => {
-          dispatch(deleteEducation(id))
-      })
-      .catch((error) => {
-          throw error
-      })
-  }
-}
+// export const deleteEducation = (id) => {
+//   return function(dispatch) {
+//       return EduApi.deleteEducation(id)
+//       .then((res) => {
+//           dispatch(deleteEducation(id))
+//       })
+//       .catch((error) => {
+//           throw error
+//       })
+//   }
+// }
