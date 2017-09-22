3abf6a8dd4dccede8ced110d6510156c199b6d46
Wendy Beck <webeck@gmail.com>
2017-09-21 18:49:09 -0700

    clearing out warnings made a little bug

--- src/edu/EduPages.js
+++ src/edu/EduPages.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {connect} from 'react-redux';
 // import {bindActionCreators} from 'redux';
 import {getEducation, updateEducation} from './actions';
