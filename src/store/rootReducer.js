c1c9190f77c5a8ded1fd51340567a4c279ecf22c
Stephanie Fitzgerald <s.e.v.fitzgerald@gmail.com>
2017-09-26 14:58:00 -0700

    Update root reducer to remove edu

--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -1,14 +1,12 @@
 import { combineReducers } from 'redux';
 import { authId } from '../app/reducers';
 import { athletes } from './athletes/reducers';
-import { educations } from '../edu/reducers';
 import { sports } from '../sports/reducers';
 import {search} from '../search/reducers';
 
 export default combineReducers({
   authId,
   athletes,
-  educations,
   sports,
   search
 });
