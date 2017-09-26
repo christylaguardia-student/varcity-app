3c70428330a1dbd2397b8034c9b473d8190a2bbf
Stephanie Fitzgerald <s.e.v.fitzgerald@gmail.com>
2017-09-26 15:51:38 -0700

    Change spacing for edu actions

--- src/edu/actions.js
+++ src/edu/actions.js
@@ -2,57 +2,54 @@ import * as actions from './constants';
 import athleteApi from '../services/athleteApi';
 
 export function makeGetEdu(api) {
-    return function getEdu(id) {
-        return (dispatch, getState) => {
-            const { athletes } = getState();
-            const athlete = athletes[id];
-            if (athlete && athlete.edu) return;
-            return api
-                .getEduById(id)
-                .then(edu => {
-                    dispatch({ type: actions.GET_EDU, payload: edu });
-                })
-                .catch(console.log);
-        };
+  return function getEdu(id) {
+    return (dispatch, getState) => {
+      const { athletes } = getState();
+      const athlete = athletes[id];
+      if (athlete && athlete.edu) return;
+      return api
+        .getEduById(id)
+        .then(edu => {
+          dispatch({ type: actions.GET_EDU, payload: edu });
+        });
     };
+  };
 }
 
 export const getEdu = makeGetEdu(athleteApi);
 
 
 export function makeUpdateSchools(api) {
-    return function updateSchools(id, data) {
-        return (dispatch, getState) => {
-            const { athletes } = getState();
-            const athlete = athletes[id];
-            if (athlete && athlete.edu) return;
-            return api
-                .updateSchoolsById(id, data)
-                .then(edu => {
-                    dispatch({ type: actions.UPDATE_SCHOOLS, payload: edu });
-                })
-                .catch(console.log);
-        };
+  return function updateSchools(id, data) {
+    return (dispatch, getState) => {
+      const { athletes } = getState();
+      const athlete = athletes[id];
+      if (athlete && athlete.edu) return;
+      return api
+        .updateSchoolsById(id, data)
+        .then(edu => {
+          dispatch({ type: actions.UPDATE_SCHOOLS, payload: edu });
+        });
     };
+  };
 }
 
 export const updateSchools = makeUpdateSchools(athleteApi);
 
 
 export function makeUpdateTestScores(api) {
-    return function updateTestScores(id, data) {
-        return (dispatch, getState) => {
-            const { athletes } = getState();
-            const athlete = athletes[id];
-            if (athlete && athlete.edu) return;
-            return api
-                .updateTestScoresById(id, data)
-                .then(edu => {
-                    dispatch({ type: actions.UPDATE_TESTSCORES, payload: edu });
-                })
-                .catch(console.log);
-        };
+  return function updateTestScores(id, data) {
+    return (dispatch, getState) => {
+      const { athletes } = getState();
+      const athlete = athletes[id];
+      if (athlete && athlete.edu) return;
+      return api
+        .updateTestScoresById(id, data)
+        .then(edu => {
+          dispatch({ type: actions.UPDATE_TESTSCORES, payload: edu });
+        });
     };
+  };
 }
 
 export const updateTestScores = makeUpdateTestScores(athleteApi);
