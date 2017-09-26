bb90427507e426d57f7c192aa3103de67680a4ec
Stephanie Fitzgerald <s.e.v.fitzgerald@gmail.com>
2017-09-26 14:31:22 -0700

    Move EDU reducers into Athlete Reducers

--- src/store/athletes/reducers.js
+++ src/store/athletes/reducers.js
@@ -1,5 +1,6 @@
 import * as actions from './constants';
 import * as mediaActions from '../../media/constants';
+import * as eduActions from '../../edu/constants';
 
 export function athletes(state = {}, { type, payload }) {
   switch(type) {
@@ -17,6 +18,22 @@ export function athletes(state = {}, { type, payload }) {
       return {
         ...state,
         [_id]: { ...athlete,  info }
+      };
+    }
+    case eduActions.GET_EDU: {
+      const { _id, edu={} } = payload;
+      const athlete = state[_id] || {};
+      return {
+        ...state,
+        [_id]: { ...athlete,  edu }
+      };
+    }
+    case eduActions.UPDATE_EDU: {
+      const { _id, edu } = payload;
+      const athlete = state[_id] || {};
+      return {
+        ...state,
+        [_id]: { ...athlete,  edu }
       };
     }
     case mediaActions.GET_MEDIA: {
