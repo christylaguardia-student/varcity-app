bb2fcaf00dbdf85a9035fd791a0c2c04ef7d5fdb
Wendy Beck <webeck@gmail.com>
2017-09-25 17:07:50 -0700

    reinstating component

--- src/sports/SportPage.js
+++ src/sports/SportPage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { connect } from 'react-redux';
 // import {bindActionCreators} from 'redux';
 import { getSport, updateSport } from './actions';
